3506a5a7d549d2ae672a75868f9ac9e9371a8159
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-01-14 19:05:08 +0100

    add other sections to homepage

--- src/components/home-content/index.tsx
+++ src/components/home-content/index.tsx
@@ -1,5 +1,7 @@
+import { useFrameContext } from "@/hooks/frame-context";
+
 import { HStack, VStack, Input, Text, Image } from "@chakra-ui/react";
-import { Button } from "@/components/ui/button";
+
 import {
   PopoverRoot,
   PopoverArrow,
@@ -8,69 +10,70 @@ import {
   PopoverTitle,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { useFrameContext } from "@/hooks/frame-context";
-import { Link, LinkBox } from "../ui/link";
-import { Header, Subtitle, Title } from "../ui/title";
-import { Paragraph, ParagraphBig } from "../ui/paragraph";
-import { Separator } from "../ui/separator";
+import { Button } from "@/components/ui/button";
+import { Link, LinkBox } from "@/components/ui/link";
+import { Header, Subtitle, Title } from "@/components/ui/title";
+import { Paragraph, ParagraphBig } from "@/components/ui/paragraph";
+import { Separator } from "@/components/ui/separator";
+
+import { Hero } from "@/components/home-content/hero";
+import { Introduction } from "@/components/home-content/introduction";
+import { GettingStarted } from "@/components/home-content/getting-started";
+import { TipsAndTricks } from "@/components/home-content/tips-tricks";
+import { HowTos } from "@/components/home-content/how-tos";
 import { Resources } from "./resources";
 
 export default function HomeContent() {
   const { context } = useFrameContext();
 
   return (
-    <VStack gap="2rem" maxW="md" mx="auto" px="2rem">
-      <HStack w="full" justifyContent="space-between" alignItems="center">
-        <Text my="4">Logo</Text>
-        <HStack alignItems="center">
-          <Image
-            src={context?.user.pfpUrl}
-            alt={`${context?.user.username} avatar`}
-            width={10}
-            height={10}
-            borderRadius="full"
-          />
-          <Text my="4">{context?.user.username}</Text>
+    <VStack gap="2rem">
+      <Hero />
+      <Introduction />
+      <GettingStarted />
+      <TipsAndTricks />
+      <HowTos />
+
+      <VStack gap="2rem" mx="auto" px="2rem">
+        <HStack w="fit-content" mx="auto" mt="4rem">
+          <Button size="xs">Click me</Button>
+          <PopoverRoot>
+            <PopoverTrigger asChild>
+              <Button size="sm" variant="outline">
+                Click me
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverBody>
+                <PopoverTitle fontWeight="medium">Naruto Form</PopoverTitle>
+                <Text my="4">
+                  Naruto is a Japanese manga series written and illustrated by
+                  Masashi Kishimoto.
+                </Text>
+                <Input placeholder="Your fav. character" size="sm" />
+              </PopoverBody>
+            </PopoverContent>
+          </PopoverRoot>
         </HStack>
-      </HStack>
-      <HStack w="fit-content" mx="auto" mt="4rem">
-        <Button size="xs">Click me</Button>
-        <PopoverRoot>
-          <PopoverTrigger asChild>
-            <Button size="sm" variant="outline">
-              Click me
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverBody>
-              <PopoverTitle fontWeight="medium">Naruto Form</PopoverTitle>
-              <Text my="4">
-                Naruto is a Japanese manga series written and illustrated by
-                Masashi Kishimoto.
-              </Text>
-              <Input placeholder="Your fav. character" size="sm" />
-            </PopoverBody>
-          </PopoverContent>
-        </PopoverRoot>
-      </HStack>
-      <Link href="/about">About</Link>
-      <Header>Heading 1</Header>
-      <Title>Heading 2</Title>
-      <Subtitle>Heading 3</Subtitle>
-      <ParagraphBig>Paragraph Big</ParagraphBig>
-      <Paragraph>Paragraph</Paragraph>
-      <Paragraph color="primary" fontWeight="semibold">
-        interactive apps
-      </Paragraph>
-      <Separator />
-      <Subtitle color="primary">Farcaster Context</Subtitle>
-      <LinkBox
-        imgSrc="./links/github.svg"
-        title="Frames v2 Demo"
-        subtitle="Forkable NextJS example app"
-        href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
-      />
+        <Link href="/about">About</Link>
+        <Header>Heading 1</Header>
+        <Title>Heading 2</Title>
+        <Subtitle>Heading 3</Subtitle>
+        <ParagraphBig>Paragraph Big</ParagraphBig>
+        <Paragraph>Paragraph</Paragraph>
+        <Paragraph color="primary" fontWeight="semibold">
+          interactive apps
+        </Paragraph>
+        <Separator />
+        <Subtitle color="primary">Farcaster Context</Subtitle>
+        <LinkBox
+          imgSrc="./links/github.svg"
+          title="Frames v2 Demo"
+          subtitle="Forkable NextJS example app"
+          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+        />
+      </VStack>
       <Resources />
     </VStack>
   );

1a75f18b4bf10626ba38f151241eee1d675ef110
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-01-16 23:37:42 +0100

    add search users in view profile

--- src/components/home-content/features/view-profile.tsx
+++ src/components/home-content/features/view-profile.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import sdk from "@farcaster/frame-sdk";
-import { VStack } from "@chakra-ui/react";
+
 import { toaster } from "@/components/ui/toaster";
+import { HStack, Input, VStack, createListCollection } from "@chakra-ui/react";
+
 import { PrimaryButton } from "@/components/ui/button";
+import { Avatar } from "@/components/ui/avatar";
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
 import { Paragraph } from "@/components/ui/paragraph";
 import CodeBlock from "@/components/ui/code-block";
+import { FarcasterUserBulkResponse, NeynarUser } from "@/lib/types";
 
 const codeBlock = `
   import { useCallback } from "react";
@@ -32,8 +44,100 @@ const codeBlock = `
   }
   `;
 
-export function ViewProfile({ fid }: { fid: number }) {
-  const viewProfile = useCallback(async () => {
+interface FarcasterSelectProps {
+  users: NeynarUser[];
+  onSelectUser: (fid: number) => void;
+}
+
+const SelectValueItem = () => (
+  <SelectValueText placeholder="Select user">
+    {(items: Array<NeynarUser>) => {
+      const { display_name, pfp_url } = items[0];
+      return (
+        <HStack>
+          <Avatar name={display_name} size="xs" src={pfp_url} />
+          {display_name}
+        </HStack>
+      );
+    }}
+  </SelectValueText>
+);
+
+const FarcasterSelect = ({ users, onSelectUser }: FarcasterSelectProps) => {
+  const handleSelect = useCallback(
+    (details: { value: string[] }) => {
+      const selectedUser = users.find(
+        (user) => user.fid.toString() === details.value[0],
+      );
+      if (selectedUser) {
+        onSelectUser(selectedUser.fid);
+      }
+    },
+    [users, onSelectUser],
+  );
+
+  const collection = createListCollection({
+    items: users,
+    itemToString: (item) => item.display_name,
+    itemToValue: (item) => item.fid.toString(),
+  });
+
+  return (
+    <SelectRoot
+      collection={collection}
+      size="sm"
+      positioning={{ sameWidth: true }}
+      onValueChange={handleSelect}
+    >
+      <SelectLabel>Select Farcaster user</SelectLabel>
+      <SelectTrigger>
+        <SelectValueItem />
+      </SelectTrigger>
+      <SelectContent portalled={false}>
+        {users.map((user) => (
+          <SelectItem item={user} key={user.fid} justifyContent="flex-start">
+            <Avatar name={user.display_name} src={user.pfp_url} size="xs" />
+            {user.display_name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </SelectRoot>
+  );
+};
+
+export function ViewProfile({ userFid }: { userFid: number }) {
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [farcasterUsers, setFarcasterUsers] = useState<NeynarUser[]>([]);
+
+  const handleSearch = async () => {
+    setIsSearching(true);
+    try {
+      const farcasterApiResult = await fetch(
+        `/api/farcaster/search?q=${searchInput}&viewer_fid=${userFid ?? 2}&limit=10`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      if (farcasterApiResult.ok) {
+        const farcasterData: FarcasterUserBulkResponse =
+          await farcasterApiResult.json();
+        setFarcasterUsers(farcasterData.data.users);
+      } else {
+        console.error("Failed to get users from farcaster");
+      }
+    } catch (error) {
+      console.error("Error searching users:", error);
+    } finally {
+      setIsSearching(false);
+    }
+  };
+
+  const viewProfile = useCallback(async (fid: number) => {
     try {
       await sdk.actions.viewProfile({ fid });
     } catch (error) {
@@ -51,7 +155,25 @@ export function ViewProfile({ fid }: { fid: number }) {
         <br />
         This will trigger a dialog on farcaster native client.
       </Paragraph>
-      <PrimaryButton onClick={viewProfile}>View your profile</PrimaryButton>
+      <PrimaryButton onClick={() => viewProfile(userFid)}>
+        View your profile
+      </PrimaryButton>
+      <VStack gap="0.5rem">
+        <HStack>
+          <Input
+            placeholder="Search Farcaster users"
+            variant="subtle"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <PrimaryButton onClick={handleSearch} loading={isSearching}>
+            {isSearching ? "Searching..." : "Search"}
+          </PrimaryButton>
+        </HStack>
+        {farcasterUsers.length > 0 && (
+          <FarcasterSelect users={farcasterUsers} onSelectUser={viewProfile} />
+        )}
+      </VStack>
       <CodeBlock
         language="typescript"
         title="ViewProfile.tsx"

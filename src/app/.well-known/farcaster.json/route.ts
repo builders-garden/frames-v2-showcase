874032bb605ae535ee57432e5cfcd98690c33a20
Matteo Midena <matteo.midena@protonmail.com>
2025-01-16 23:55:24 +0100

    chore: update manifest signatures

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,23 +1,24 @@
 export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_URL;
+  const appUrl =
+    process.env.NEXT_PUBLIC_URL || "https://frames-v2.builders.garden";
 
   const config = {
     accountAssociation: {
       header:
-        "eyJmaWQiOjU2OTgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg5Qzk1QjM1M2MxMDM5RTQzNUM5MTE1NzM3MjBhNGU2MmY4ZTBhYmNhIn0",
-      payload: "eyJkb21haW4iOiI5Y2JlLTIxMy0xODctMjMtMTQubmdyb2stZnJlZS5hcHAifQ",
+        "eyJmaWQiOjI2MjgwMCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDEwN2IyYzM1MEEzRDMxZDUzYTFGNzY1ODU0NUU4ZEIxNTBEMGQ5RTQifQ",
+      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIuYnVpbGRlcnMuZ2FyZGVuIn0",
       signature:
-        "MHhhOTA0YjBkNWZlOThhODExYjkxMzQwNDNlMzBkZmMyNjE2NzM2YzZkNGQ0Yzc3MzFiMTY5NGE0NDExODQ2YWJjNWZiM2Y1MjBmZDdhNTI0YTcxZWE2OGI2NTM5OTI4Yzg0OGRhNjNkYzViNDlhZmI1YmJiODI4MjJjM2U1ZjRkMzFj",
+        "MHhlZDc5N2QxNDdiNDFkMGJkNDJkOTc2NDkxODUyYjhmMmNmNTQ4ZGIwZGRiZDc0YmIyOTc5ZTQzN2E2YzcwNzg4NDY1YzU0NGU0OTc1NWVmN2Y4YzYyODIzODliMGNjNTM3NzAwMTVlMDgxMGRjNzgwNDM1ZTA0ZmI2MzY2N2ZjZTFi",
     },
     frame: {
       version: "1",
-      name: "Farcaster FramesV2 Showcase",
+      name: "Speedrun frames v2",
       iconUrl: `${appUrl}/icon.png`,
       homeUrl: `${appUrl}`,
-      imageUrl: `${appUrl}/image.png`,
-      buttonTitle: `FramesV2 Showcase`,
+      imageUrl: `${appUrl}/splash.png`,
+      buttonTitle: `Speedrun frames v2`,
       splashImageUrl: `${appUrl}/splash.png`,
-      splashBackgroundColor: `#eeccff`,
+      splashBackgroundColor: `#402e75`,
       webhookUrl: `${appUrl}/api/webhook`,
     },
   };
